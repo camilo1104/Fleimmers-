b540cf34695125d743a62d665b7465900ddc950f
andres camilo sanchez <camilosanchez2002@gmail.com>
2020-05-12 18:30:45 -0500

    Se agrego el archivo videojuegos.java

---
+++ videojuegos.java
@@ -0,0 +1,21 @@
+package proyecto.integrador;
+
+
+public class videoJuegos {
+    private String nombre;
+    private char controles;
+    private int cantidadJugadores;
+
+    public videoJuegos(String nombre, char controles, int cantidadJugadores) {
+        this.nombre = nombre;
+        this.controles = controles;
+        this.cantidadJugadores = cantidadJugadores;
+    }
+
+    public void reconocerCantidadJugadores(){
+
+    }
+    public void asignarControles(){
+
+    }
+}
